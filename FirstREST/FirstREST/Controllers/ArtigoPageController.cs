7387c104a3c9766d0f708cfba3ade3c29f4154e2
AndreRegado <andreregado4@gmail.com>
2015-11-17 23:08:52 +0000

    list product (begining)

--- FirstREST/FirstREST/Controllers/ArtigoPageController.cs
+++ FirstREST/FirstREST/Controllers/ArtigoPageController.cs
@@ -3,25 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Http;
 
 namespace FirstREST.Controllers
 {
+    [HandleError]
     public class ArtigoPageController : Controller
     {
 
-        //
         // GET: /ArtigoPage/
-
-        public ActionResult Index()
+        [System.Web.Mvc.HttpGet]
+        public ActionResult Index(string id)
         {
+            //string idString = id.ToString();
 
-            Lib_Primavera.Model.Artigo artigo = Lib_Primavera.PriIntegration.GetArtigo("A0002");
+            Lib_Primavera.Model.Artigo artigo = Lib_Primavera.PriIntegration.GetArtigo(id);
 
             ViewBag.id = artigo.ID;
             ViewBag.preco = artigo.Preço;
             return View();
         }
 
-        
+       
+
+   
     }
+
 }
