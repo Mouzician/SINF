ac79c5a73f00a35747608d5a136e0630382e92c1
Pedro Sousa <p4sousa@gmail.com>
2015-12-15 23:50:10 +0000

    pls

--- FirstREST/FirstREST/Scripts/auxx.js
+++ FirstREST/FirstREST/Scripts/auxx.js
@@ -1,24 +1,26 @@
-  function Increase(nome) {
-      
+function Increase(nome) {
+
     var y = document.getElementById(nome).innerHTML;
+    var eleme = document.getElementById("hidden " +nome);
     var z = document.getElementById(nome + " + price").innerHTML;
     var x = document.getElementById(nome + " + hide").value;
-      
-    document.getElementById(nome).innerHTML = Number(y) + 1 ;
+
+    document.getElementById(nome).innerHTML = Number(y) + 1;
+    eleme.value = Number(y) + 1;
     document.getElementById(nome + " + price").innerHTML = Number(z) + Number(x);
-      
-          var sum = 0;
-          var qt = 0;
 
-        $('.item__price').each(function(){
+    var sum = 0;
+    var qt = 0;
+
+    $('.item__price').each(function () {
         sum += parseFloat($(this).text());  //Or this.innerHTML, this.innerText
-        });
+    });
 
-        $(".sum").text(sum.toFixed(2));
-        $(".taxes").text((sum*0.23).toFixed(2));
-        $(".total").text((sum + sum*0.23).toFixed(2));
+    $(".sum").text(sum.toFixed(2));
+    $(".taxes").text((sum * 0.23).toFixed(2));
+    $(".total").text((sum + sum * 0.23).toFixed(2));
 
-      }
+}
 
  function Decrease(nome) {
      
